e64be775dbdb8f6510ce1526e1bf72e94edee48d
ipcampbell12 <ipcampbell12@gmail.com>
2023-06-17 15:43:26 -0700

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const Joi = require('joi')
 const express = require('express')
+const cors = require('cors')
 const people = require('./Routes/people')
 const morgan = require('morgan')
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json());
 app.use(morgan('tiny'));
