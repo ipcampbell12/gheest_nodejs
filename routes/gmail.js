23bec257e268b1eeae112b827e7346cd2d27056d
ipcampbell12 <ipcampbell12@gmail.com>
2023-06-30 12:19:49 -0700

    added gmail api routes

--- routes/gmail.js
+++ routes/gmail.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const controllers = require('./controllers')
 const router = express.Router();
 
 require("dotenv").config();
 
 router.get("/", async (req, res) => {
     res.send("welcome to Gmail API with NodeJS")
-})
+});
+
+router.get('/mail/user/:email', controllers.getUser)
+router.get('/mail/send', controllers.sendMail)
+router.get('/mail/drafts/:email', controllers.getDrafts)
+router.get('/mail/read/:messageId', controllers.readMail);
+
+module.exports = router;
