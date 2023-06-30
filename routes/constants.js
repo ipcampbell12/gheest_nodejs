2a8f6f791d2140ce45854761ecd418cbb0addcbf
ipcampbell12 <ipcampbell12@gmail.com>
2023-06-30 12:10:56 -0700

    added constants and utilites for gmail api

---
+++ routes/constants.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+
+const auth = {
+    type: "OAuth2",
+    user: "ipcampbell12@gmail.com",
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN
+};
+
+const mailOptions = {
+    from: "Ian, <ipcampbell12@gmail.com>",
+    to: "ipcampbell12@gmail.com>",
+    subject: "Gmail API NodeJS"
+};
+
+module.exports = { auth, mailOptions }
