a1bf4722c7d2d20ce4994020e3c70dc90f5d7c53
ipcampbell12 <ipcampbell12@gmail.com>
2023-06-30 17:42:10 -0700

    removed some comments

--- helper_functions/shiftId.js
+++ helper_functions/shiftId.js
@@ -4,14 +4,8 @@ async function update(obj) {
     const arr = Object.values(obj)
     const val = arr.pop()
     arr.unshift(val)
-    //console.log(arr)
     const now = new Date().toLocaleString();
     arr.push(now)
-
-    // const timeStamp = new Date().getDate();
-    // arr.append(timeStamp);
-    // console.log(arr)
-    //console.log(arr)
     // const summary = await summarizeText(arr[1])
     // arr.append(summary)
     // const splicedArr = arr.toSpliced(4, 1, summary)
